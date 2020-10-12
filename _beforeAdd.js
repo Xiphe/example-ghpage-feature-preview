5b5ae85f7ba2af950636cca7f43856206efb3961
Xiphe <github@xiphe.net>
2020-10-12 14:32:58 +0200

    chore: add cleanup script

---
+++ _beforeAdd.js
@@ -0,0 +1,29 @@
+const { readdir, rmdir } = require("fs").promises;
+const path = require("path");
+
+const REMOTE = "origin";
+
+/** @param git {import("gh-pages/lib/git")} */
+module.exports = async (git) => {
+  await git.exec("remote", "set-branches", REMOTE, "*");
+  await git.fetch("--all");
+  await git.exec("branch", "-r", "--no-merged", REMOTE + "/main");
+
+  const previewFolder = path.join(git.cwd, "preview");
+  const unmergedRemoteBranches = git.output
+    .trim()
+    .split("\n")
+    .map((branchName) =>
+      branchName.trim().replace(new RegExp(`^${REMOTE}\/`, ""), "")
+    );
+  const previews = await readdir(previewFolder);
+  const mergedOrDeletedPreviews = previews.filter(
+    (previewName) => !unmergedRemoteBranches.includes(previewName)
+  );
+
+  return Promise.all(
+    mergedOrDeletedPreviews.map((previewName) =>
+      rmdir(path.join(previewFolder, previewName), { recursive: true })
+    )
+  );
+};
